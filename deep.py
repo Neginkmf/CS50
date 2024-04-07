87dae7cbbc88555e79ee3da9b4723a7dec7ab5fb
Negin Kabirmokhtarfar <161232666+Neginkmf@users.noreply.github.com>
2024-04-07 12:16:22 +0330

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,5 @@
+x = input("what's the answer to the Great Question of Life, the Universe and Everything? ")
+if x == "42" or x == "forty_two" or x == "forty two":
+    print("Yes")
+else:
+    print("No")
