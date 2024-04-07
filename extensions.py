d1bd91a28d4e9b436aff5fc2b1a1010f9cb640ae
Negin Kabirmokhtarfar <161232666+Neginkmf@users.noreply.github.com>
2024-04-07 12:15:45 +0330

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,17 @@
+file_name=input("what's the file's name?").lower()
+if file_name.endswith(".gif"):
+    print("image/gif")
+elif file_name.endswith(".jpg"):
+    print("image/jpg")
+elif file_name.endswith(".jpeg"):
+    print("image/jpeg")
+elif file_name.endswith(".png"):
+    print("image/png")
+elif file_name.endswith(".pdf"):
+    print("application/pdf")
+elif file_name.endswith(".txt"):
+    print("text/plain")
+elif file_name.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
